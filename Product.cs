16d59ff7f98536963a8a9cd8cea57a993477d440
Aniket <96255990+Aniketpabale@users.noreply.github.com>
2022-02-19 19:18:35 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinalExam_KH.Models
+{
+    public class Product
+    {
+        [Display(Name = "Product Id")]
+        [Required(ErrorMessage="please enter product id")]
+        public int ProdctId { get; set; }
+
+        [Display(Name ="Product Name")]
+        [Required(ErrorMessage ="please Enter Product Name ")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Rate")]
+        [Required(ErrorMessage = "please Enter Rate ")]
+        public decimal Rate { get; set; }
+
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "please Enter Description ")]
+        public string Description { get; set; }
+
+        [Display(Name = "Category")]
+        [Required(ErrorMessage = "please Enter Category ")]
+        public string Category { get; set; }
+
+
+
+
+
+
+    }
+}
